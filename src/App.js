7a209c014139d47f94414816903989d2830e511c
fmacedosantos <felipemacedo986@gmail.com>
2024-08-09 12:34:34 -0300

    Refrescando as ideias de React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import GoalList from './components/GoalList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  
+const courseGoals = [
+  {id: 'cg1', text: 'Finish the course'},
+  {id: 'cg2', text: 'Learn all about the course main topic'},
+  {id: 'cg3', text: 'Help other students in the course Q&A'},
+]
+
+  return <div className='course-goals'>
+    <h2>Course goals</h2>
+    <GoalList goals={courseGoals} />
+  </div>
 }
 
 export default App;
