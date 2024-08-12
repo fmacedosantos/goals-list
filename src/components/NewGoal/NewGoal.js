7ca4b6f8a115279c689312564f0a05fa9ecd9230
fmacedosantos <felipemacedo986@gmail.com>
2024-08-12 11:00:44 -0300

    Manipulando o input sem state

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -3,19 +3,25 @@ import React from 'react'
 import './NewGoal.css';
 
 const NewGoal = props => {
+    let enteredText = '';
+
     const addGoalHandler = event => {
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'My new goal'
+            text: enteredText
         };
 
         props.onAddGoal(newGoal);
     }
 
+    const textChangeHandler = event => {
+        enteredText = event.target.value
+    }
+
     return <form className='new-goal' onSubmit={addGoalHandler}>
-        <input type='text'/>
+        <input type='text' onChange={textChangeHandler}/>
         <button type='submit'>Add Goal</button>
     </form>
 };
